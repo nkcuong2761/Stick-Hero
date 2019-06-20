c59268c9e309c3d5172353428eb45675f9926e9a
bachtran69 <victor.tnbach@gmail.com>
2019-06-20 21:36:54 +0700

    Delete KeyEventPress.java

--- Stick Hero/src/Game/KeyEventPress.java
+++
@@ -1,7 +0,0 @@
-package Game;
-
-public class KeyEventPress {
-    public static boolean isStickPress = false;
-    public static boolean isRPress = false;
-    public static boolean isSpaceJusRelease = false;
-}
